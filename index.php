2fb2780d8937ef7b37d58936c3913f632cecdb60
Clay Walker <ClaytonWalker11@gmail.com>
2012-05-20 22:31:33 -0600

    Removed a file, added db Dump

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once ('utility/master.php');
+
+$sql="Select ";
+
+?>
+<p>index body</p>
+</body>
+	<p>This is the body!</p>
+</body>
+</html>
---
+++ utility/master.php
@@ -0,0 +1,10 @@
+<?php
+include_once('db.php');
+?>
+
+<html>
+<head>
+	<title>Site title</title>
+	<script type='text/javascript' src='/js/jquery-1.4.4.min.js'>
+	</script>
+</head>

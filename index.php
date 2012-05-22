c287a111cece436ad9f9f0cdaf603d31ca2f02f9
Clay Walker <ClaytonWalker11@gmail.com>
2012-05-21 19:34:04 -0600

    changed flow to try out Joeys suggestion. added JQuery datepicker, against Joeys recommendation

--- index.php
+++ index.php
@@ -7,7 +7,22 @@ if(isset($_GET['building']))
 		$building_ID = $_GET['building'];
 	}
 ?>
-</body>
+<body>
+
+	<p>Please select the number of people in your party: 
+		<select>
+			<option value='1'>1-4</option>
+			<option value='2'>5-8</option>
+			<option value='3'>8-10</option>
+			<option value='4'>10+</option>
+		</select>
+	</p>
+
+		<p>Please select a date, time and duration: </p>
+<div class="dateTimeSelection">
+<p>Date: <input type="text" id="datepicker" size="30"/></p>
+</div>
+
 	<p>Please select a building: </p>
 	<form method="GET" action="">
 		<?php getBuildings(); ?>
--- utility/master.php
+++ utility/master.php
@@ -1,8 +1,21 @@
 <?php
 include_once('db.php');
 ?>
+<meta charset="utf-8">
 <html>
 <head>
 	<title>UReserve</title>
-    <script language='JavaScript' type='text/JavaScript' src='js/main.js'></script>
+	<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+    <script language='JavaScript' type='text/JavaScript' src='js/jquery-ui-1.8.20.custom.min.js'></script>
+	<link type="text/css" href="css/smoothness/jquery-ui-1.8.20.custom.css" rel="Stylesheet" />
+
+
+    <script>
+	$(function() {
+		$( "#datepicker" ).datepicker();
+		$( "#format" ).change(function() {
+			$( "#datepicker" ).datepicker( "option", "dateFormat", $( this ).val() );
+		});
+	});
+	</script>
 </head>
